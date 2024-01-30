2e07f4c341929ce40e188921f984a5c1134c504b
WangYuHan <1269940762@qq.com>
2024-01-30 13:46:08 +0800

    modify class ForwardRender

--- OpenGL/Render/ForwardRenderer.cpp
+++ OpenGL/Render/ForwardRenderer.cpp
@@ -1,4 +1,6 @@
 #include "ForwardRenderer.h"
+#include "RenderTarget.h"
+#include "../Camera/FlyCamera.h"
 
 void ForwardRenderer::DoRender()
 {
@@ -7,10 +9,24 @@ void ForwardRenderer::DoRender()
 
 void ForwardRenderer::OnReSize(uint32 width, uint32 height)
 {
-
+	renderTarget->Resize(width, height);
 }
 
 void ForwardRenderer::Initialize()
 {
-	
+	cam = new FlyCamera(glm::vec3(0.0f, 1.0f, 0.0f), glm::vec3(0.0f, 0.0f, -1.0f));
+	renderTarget = new RenderTarget(2048, 2048, GL_UNSIGNED_BYTE, 1, true);
+}
+
+void ForwardRenderer::Clear()
+{
+	Clear(true, true, true);
+}
+
+void ForwardRenderer::Clear(bool p_colorBuffer, bool p_deptBuffer, bool p_stencilColor)
+{
+	glClear(p_colorBuffer ? GL_COLOR_BUFFER_BIT : 0 |
+		p_deptBuffer ? GL_DEPTH_BUFFER_BIT : 0 |
+		p_stencilColor ? GL_STENCIL_BUFFER_BIT : 0
+	);
 }
--- OpenGL/Render/ForwardRenderer.h
+++ OpenGL/Render/ForwardRenderer.h
@@ -1,4 +1,7 @@
 #pragma once
+
+class RenderTarget;
+
 #include "BaseRenderer.h"
 class ForwardRenderer :
     public BaseRenderer
@@ -7,5 +10,15 @@ public:
 	virtual void DoRender() override;
 	virtual void OnReSize(uint32 width, uint32 height) override;
 	virtual void Initialize() override;
+	virtual void Clear() override;
+
+protected:
+
+
+	void Clear(bool p_colorBuffer = true, bool p_deptBuffer = true, bool p_stencilColor = true);
+	
+private:
+	RenderTarget* renderTarget;
+	Camera* cam;
 };
 
