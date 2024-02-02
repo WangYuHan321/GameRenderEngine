9ebbfef6df57a43585a29533150de83335078d27
WangYuHan <1269940762@qq.com>
2024-02-02 17:30:55 +0800

    add widget DragSingleScalar class .

---
+++ OpenGL/UI/Widgets/Drag/DragSingleScalar.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include "../../Event/Event.h"
+#include "../DataWidget.h"
+
+template< typename T>
+class DragSingleScalar : public DataWidget<T>
+{
+	static_assert(std::is_scalar<T>::value, "Invalid DragSingleScalar T (Scalar expected)");
+
+public:
+	DragSingleScalar(
+		ImGuiDataType p_dataType,
+		T p_min,
+		T p_max,
+		T p_value,
+		float p_speed,
+		const std::string& p_label,
+		const std::string& p_format
+	):DataWidget<T>(value), m_dataType(p_dataType), min(p_min), max(p_max), value(p_value), speed(p_speed), label(p_label), format(p_format) {}
+
+
+	void _Draw_Impl() override
+	{
+		if (max < min)
+			max = min;
+
+		if (value < min)
+			value = min;
+		else if (value > max)
+			value = max;
+
+		if (ImGui::DragScalar((label + this->m_widgetID).c_str(), m_dataType, &value, speed, &min, &max, format.c_str()))
+		{
+			ValueChangedEvent.Invoke(value);
+			this->NotifyChange();
+		}
+	}
+
+public:
+	T min;
+	T max;
+	T value;
+	float speed;
+	std::string label;
+	std::string format;
+	Event<T> ValueChangedEvent;
+
+private:
+	ImGuiDataType m_dataType;
+};
+
