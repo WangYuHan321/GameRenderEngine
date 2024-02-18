21b485f888b30bf34c896d48a6111b6c230e7600
WangYuHan <1269940762@qq.com>
2024-02-19 06:15:03 +0800

    add widget Separator class

---
+++ OpenGL/UI/Visual/Separator.cpp
@@ -0,0 +1,6 @@
+#include "Separator.h"
+
+void Separator::_Draw_Impl()
+{
+	ImGui::Separator();
+}
---
+++ OpenGL/UI/Visual/Separator.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "../Widgets/AWidget.h"
+class Separator : public AWidget
+{
+protected:
+	void _Draw_Impl() override;
+};
+
