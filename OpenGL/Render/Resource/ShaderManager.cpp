b1c521768af931f12f1d35b5cbd21510d9170258
WangYuHan <1269940762@qq.com>
2024-02-21 17:20:27 +0800

    modify shadermgr .

--- OpenGL/Render/Resource/ShaderManager.cpp
+++ OpenGL/Render/Resource/ShaderManager.cpp
@@ -1,10 +1,18 @@
 #include "ShaderManager.h"
 
+#include "Loader/ShaderLoader.h"
+
  CShader* ShaderManager::CreateResource(const std::string& p_path)
 {
+	 CShader* pShader = ShaderLoader::getInstance()->Create(p_path);
+
+	 if (pShader)
+		 return pShader;
+
 	 return nullptr;
 }
 
  void ShaderManager::DestroyResource(const std::string& p_path)
 {
+
 }
