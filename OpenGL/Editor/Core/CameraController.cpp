53b8449c7d6a396a78fb5a4a51d71490304b43dd
WangYuHan <1269940762@qq.com>
2024-02-27 19:12:53 +0800

    commit CameraController input function

--- OpenGL/Editor/Core/CameraController.cpp
+++ OpenGL/Editor/Core/CameraController.cpp
@@ -1,5 +1,6 @@
 #include "CameraController.h"
 
+#include <math.h>
 #include <algorithm>
 #include "../Core/EditorAction.h"
 #include "../../Core/ECS/Actor.h"
@@ -45,11 +46,82 @@ float GetActorFocusDist(Actor& p_actor)
 	return distance;
 }
 
+Vector3 RemoveRoll(const Vector3& p_ypr)
+{
+	Vector3 result = p_ypr;
+
+	if (result.z >= 179.0f || result.z <= -179.0f)
+	{
+		result.x += result.z;
+		result.y = 180.0f - result.y;
+		result.z = 0.0f;
+	}
+
+	if (result.x > 180.0f) result.x -= 360.0f;
+	if (result.x < -180.0f) result.x += 360.0f;
+
+	return result;
+}
 
 void CameraController::HandleInputs(float p_deltaTime)
 {
 	if (m_view.IsHovered())
 	{
+		if (!m_cameraDestinantion.empty())
+		{
+			auto& [destPos, destRotation] = m_cameraDestinantion.front();
+			m_cameraDestinantion.pop();
+			m_camera.Position = destPos;
+			m_camQuat = destRotation;
+		}
 
+		if (m_inputMgr.IsMouseButtonReleased(EMouseButton::MOUSE_BUTTON_RIGHT))
+		{
+			m_mouseIsRightPresssed = true;
+		}
+
+		if (m_inputMgr.IsMouseButtonPressed(EMouseButton::MOUSE_BUTTON_RIGHT))
+		{
+			if (m_mouseIsRightPresssed)
+			{
+				m_newDelta = m_inputMgr.GetMousePosition();
+				m_oldDelta = m_newDelta;
+				m_mouseIsRightPresssed = false;
+			}
+			else
+			{
+				m_newDelta = m_inputMgr.GetMousePosition();
+				std::pair<double, double>  diff = { m_oldDelta.first - m_newDelta.first,
+					m_oldDelta.second - m_newDelta.second };
+				HandleCameraFPSMouse(Vector2(diff.first, diff.second), true);
+			}
+		}
+
+
+		if (m_inputMgr.IsKeyPressed(EKey::KEY_W)) { m_camPos = m_camPos + m_camera.Forward; }
+		if (m_inputMgr.IsKeyPressed(EKey::KEY_S)) { m_camPos = m_camPos - m_camera.Forward; }
+		if (m_inputMgr.IsKeyPressed(EKey::KEY_A)) { m_camPos = m_camPos + m_camera.Right; }
+		if (m_inputMgr.IsKeyPressed(EKey::KEY_D)) { m_camPos = m_camPos - m_camera.Right; }
 	}
+}
+
+void CameraController::HandleCameraFPSMouse(Vector2 p_mouseOffset, bool isMouseFirst)
+{
+	auto mouseOffset = p_mouseOffset * m_mouseSensitivity;
+
+	Vector3 ypr = EulerAngles(m_camQuat);
+	if (isMouseFirst)
+	{
+		ypr = RemoveRoll(ypr);
+	}
+
+	ypr.y -= mouseOffset.x;
+	ypr.x += -mouseOffset.y;
+	ypr.x = std::max(std::min(ypr.x, 90.0f), -90.0f);
+	m_camQuat = Vector3ToQuat(ypr);
+}
+
+void CameraController::MoveToTarget(Actor& p_target)
+{
+	m_cameraDestinantion.push({ p_target.m_transform.GetWorldPosition() - m_camQuat * Vector3(0.0f, 0.0f, 1.0f) * GetActorFocusDist(p_target), m_camQuat });
 }
--- OpenGL/Editor/Core/CameraController.h
+++ OpenGL/Editor/Core/CameraController.h
@@ -1,9 +1,11 @@
 #pragma once
 
+#include<queue>
 #include "../Panels/AView.h"
 
 class Context;
 class Window;
+class Actor;
 class InputManager;
 
 class CameraController
@@ -17,6 +19,11 @@ public:
 
 	void HandleInputs(float p_deltaTime);
 
+	void MoveToTarget(Actor& p_actor);
+
+private:
+	void HandleCameraFPSMouse(Vector2 mouseOffset, bool isMouseFirst);
+
 private:
 	Window& m_window;
 	AView& m_view;
@@ -25,6 +32,15 @@ private:
 	Vector3& m_camPos;
 	Quaternion& m_camQuat;
 
+	std::queue<std::tuple<Vector3, Quaternion>> m_cameraDestinantion;
+
+	// Û±Í”“ª˜øÿ÷∆
+	std::pair<double, double> m_oldDelta;
+	std::pair<double, double> m_newDelta;
+	bool m_mouseIsRightPresssed = true;
+	float m_mouseSensitivity = 0.02f;
+	// Û±Í”“ª˜øÿ÷∆
+	
 	InputManager& m_inputMgr;
 };
 
