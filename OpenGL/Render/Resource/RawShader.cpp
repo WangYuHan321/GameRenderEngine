fe16fc0d881203b71869f42e2426825801127670
WangYuHan <1269940762@qq.com>
2024-03-05 14:53:24 +0800

    add selected actor and gizmo.

--- OpenGL/Render/Resource/RawShader.cpp
+++ OpenGL/Render/Resource/RawShader.cpp
@@ -107,3 +107,124 @@ void main()
 )";
 	return source;
 }
+
+std::pair<std::string, std::string> RawShader::GetGrizmo()
+{
+	std::pair<std::string, std::string> source;
+
+	source.first = R"(
+#version 430 core
+
+layout (location = 0) in vec3 geo_Pos;
+layout (location = 1) in vec2 geo_TexCoord;
+layout (location = 2) in vec3 geo_Normal;
+
+layout (std140) uniform EngineUBO
+{
+	mat4 ubo_Model;
+	mat4 ubo_View;
+	mat4 ubo_Projection;
+	vec3 ubo_ViewPos;
+	float ubo_Time; 
+};	
+
+out VS_OUT
+{
+	vec3 Color;
+} vs_out;
+
+uniform bool u_IsBall; //球 
+uniform bool u_IsPickable; //被鼠标选择并可以移动
+uniform int u_HighlightedAxis;//高亮的移动轴
+
+mat4 rotationMatrix(vec3 axis, float angle)
+{
+	axis = normalize(axis);
+    float s = sin(angle);
+    float c = cos(angle);
+    float oc = 1.0 - c;
+    
+    return mat4
+    (
+        oc * axis.x * axis.x + c,           oc * axis.x * axis.y - axis.z * s,  oc * axis.z * axis.x + axis.y * s,  0.0,
+        oc * axis.x * axis.y + axis.z * s,  oc * axis.y * axis.y + c,           oc * axis.y * axis.z - axis.x * s,  0.0,
+        oc * axis.z * axis.x - axis.y * s,  oc * axis.y * axis.z + axis.x * s,  oc * axis.z * axis.z + c,           0.0,
+        0.0,                                0.0,                                0.0,                                1.0
+    );
+}
+
+void main()
+{
+	mat4 instanceModel = ubo_Model;
+	
+	if (gl_InstanceID == 1)
+		instanceModel = instanceModel * rotationMatrix(vec3(0, 1, 0), radians(-90)); //X
+	else if (gl_InstanceID == 2)
+		instanceModel = instanceModel * rotationMatrix(vec3(1, 0, 0), radians(90)); //Y
+
+	float distanceToCamera = distance(ubo_ViewPos, vec3(instanceModel[0][3], instanceModel[1][3], instanceModel[2][3]));
+
+	vec3 pos = geo_Pos;
+
+	vec3 fragPos = vec3(instanceModel * vec4(pos * distanceToCamera * 0.1f, 1.0f));
+
+	if(u_IsPickable)//如果当前是选中
+	{
+		int blueComponent = 0;
+		
+		if(gl_InstanceID == 1)
+			blueComponent = 255;
+	
+		if(gl_InstanceID == 2)
+			blueComponent = 253;	
+
+		if(gl_InstanceID == 0)
+			blueComponent = 254;
+		
+		vs_out.Color = vec3(1.0f, 1.0f, blueComponent / 255.0f);
+	}
+	else
+	{
+		if(u_IsBall)
+		{
+			vs_out.Color = vec3(1.0f);
+		}
+		else
+		{
+			float red	= float(gl_InstanceID == 1); // X
+			float green = float(gl_InstanceID == 2); // Y
+			float blue	= float(gl_InstanceID == 0); // Z
+
+			//if (!u_IsPickable && ((gl_InstanceID == 1 && u_HighlightedAxis == 0) || (gl_InstanceID == 2 && u_HighlightedAxis == 1) || (gl_InstanceID == 0 && u_HighlightedAxis == 2)))
+			//{
+			//	vs_out.Color = vec3(1.0f, 1.0f, 0.0f);
+			//}	
+			//else
+			//{
+				vs_out.Color = vec3(red, green, blue);
+			//}
+		}
+	}
+	    gl_Position = vec4(fragPos, 1.0) * ubo_View * ubo_Projection;
+}
+		)";
+
+	source.second = R"(
+	#version 430 core
+
+	out vec4 FRAGMENT_COLOR;
+
+	in VS_OUT
+	{
+		vec3 Color;
+	} fs_in;
+
+	uniform bool u_IsPickable;
+
+	void main()
+	{
+		FRAGMENT_COLOR = vec4(fs_in.Color, 1.0f);
+	})";
+	
+	return source;
+}
--- OpenGL/Render/Resource/RawShader.h
+++ OpenGL/Render/Resource/RawShader.h
@@ -7,5 +7,7 @@ class RawShader
 public:
 	static std::pair<std::string, std::string> GetGrid();
 
+	static std::pair<std::string, std::string> GetGrizmo();
+
 };
 
