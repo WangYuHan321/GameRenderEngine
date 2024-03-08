5fa77cc7b62181eac1edf4ab8bb660a356e9f98c
WangYuHan <1269940762@qq.com>
2024-03-08 14:13:11 +0800

    add new ToolBar class.

---
+++ OpenGL/Editor/Panels/ToolBar.cpp
@@ -0,0 +1,20 @@
+#include "ToolBar.h"
+
+//ToolBar::ToolBar
+//(
+//	const std::string& p_title,
+//	bool p_opened,
+//	const PanelWindowSetting& p_panelSetting
+//):
+//	PanelWindow(p_title, p_opened, p_panelSetting)
+//{
+//	//m_playButton = &CreateWidget<ButtonImage>()
+//
+//
+//}
+//
+//
+//void ToolBar::_Draw_Impl()
+//{
+//
+//}
---
+++ OpenGL/Editor/Panels/ToolBar.h
@@ -0,0 +1,27 @@
+#pragma once
+
+//#include "../../UI/Panel/PanelWindow.h"
+//#include "../../UI/Widgets/Button/ButtonImage.h"
+//
+//class ToolBar : public PanelWindow
+//{
+//public:
+//
+//	ToolBar
+//	(
+//		const std::string& p_title,
+//		bool p_opened,
+//		const PanelWindowSetting& p_panelSetting
+//	);
+//
+//	void _Draw_Impl() override;
+//
+//private:
+//
+//	ButtonImage* m_playButton;
+//	ButtonImage* m_pauseButton;
+//	ButtonImage* m_stopButton;
+//	ButtonImage* m_nextButton;
+//
+//};
+
