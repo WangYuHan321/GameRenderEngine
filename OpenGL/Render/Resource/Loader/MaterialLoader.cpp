b3473ba0e75d1c302a6ffc8b63d25379624e9766
WangYuHan <1269940762@qq.com>
2024-03-12 17:20:56 +0800

    modify materialloader create funciton .

--- OpenGL/Render/Resource/Loader/MaterialLoader.cpp
+++ OpenGL/Render/Resource/Loader/MaterialLoader.cpp
@@ -1,4 +1,6 @@
 #include "MaterialLoader.h"
+#include "ShaderLoader.h"
+#include "../../Core/API/ISerializable.h"
 
 Material MaterialLoader::LoadMaterial(std::string name, CShader* p_shader)
 {
@@ -14,6 +16,19 @@ Material MaterialLoader::LoadMaterial(CShader* p_shader)
 
 Material* MaterialLoader::Create(const std::string& p_path)
 {
+	tinyxml2::TinyXMLDocument doc;
+	doc.LoadFile(p_path.c_str());
+
+	if (!doc.Error())
+	{
+		tinyxml2::XMLNode* root = doc.FirstChild();
+
+		Material* pMaterial = new Material();
+#ifdef  USE_EDITOR
+		pMaterial->OnDeserialize(doc, root);
+#endif
+		return pMaterial;
+	}
 	return nullptr;
 }
 
