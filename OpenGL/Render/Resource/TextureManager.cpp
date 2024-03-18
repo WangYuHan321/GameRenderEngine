fbfc6b1667ea84633dadc7961119564951008e41
WangYuHan <1269940762@qq.com>
2024-03-18 11:35:43 +0800

    modify TextureMgr load

--- OpenGL/Render/Resource/TextureManager.cpp
+++ OpenGL/Render/Resource/TextureManager.cpp
@@ -1,10 +1,12 @@
 #include "TextureManager.h"
+#include "../../File/ConfigManager.h"
 #include "Loader/TextureLoader.h"
 
 Texture* TextureManager::CreateResource(const std::string& p_path)
 {
-	string realPath = __ENGINE_ASSET_PATH + p_path;
-	return nullptr;
+	string realPath = ConfigManager::getInstance()->GetEditorPath() + "\\Texture\\" + p_path;
+	Texture* pTexture = TextureLoader::getInstance()->LoadTexture(realPath, GL_TEXTURE_2D, GL_RGBA);
+	return pTexture;
 }
 
 
