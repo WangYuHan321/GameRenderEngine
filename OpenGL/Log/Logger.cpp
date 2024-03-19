9f5c0bad8ce0e4ad6291d35fc76d27e7f1ccbbe9
WangYuHan <1269940762@qq.com>
2024-03-19 22:49:47 +0800

    modify Logger class

--- OpenGL/Log/Logger.cpp
+++ OpenGL/Log/Logger.cpp
@@ -4,16 +4,17 @@
 void Logger::Log(const std::string& p_message, LogLevel p_logLevel,
 	LogMode p_logMode, std::string p_handlerId)
 {
-	LogData logData{ p_message, p_logLevel, Date::GetDateAsString() };
+	LogData logDatas{ p_message, p_logLevel, Date::GetDateAsString() };
+	m_logData.push_back(logDatas);
 
 	switch (p_logMode)
 	{
 	case LogMode::Default:
-	case LogMode::Console: LogToHandlerMap<ConsoleHandler>(Console_Handle_Map, logData, p_handlerId); break;
+	case LogMode::Console: LogToHandlerMap<ConsoleHandler>(Console_Handle_Map, logDatas, p_handlerId); break;
 	case LogMode::File:		
 	case LogMode::History: 	
 	case LogMode::All:
-		LogToHandlerMap<ConsoleHandler>(Console_Handle_Map, logData, p_handlerId);
+		LogToHandlerMap<ConsoleHandler>(Console_Handle_Map, logDatas, p_handlerId);
 		break;
 	}
 	//LogEvent.Invoke(logData);
@@ -29,4 +30,9 @@ ConsoleHandler& Logger::CreateConsoleHandler(std::string p_id)
 ConsoleHandler& Logger::GetConsoleHandler(std::string p_id)
 {
 	return Console_Handle_Map[p_id];
+}
+
+std::vector<LogData> Logger::GetAllLogData()
+{
+	return m_logData;
 }
--- OpenGL/Log/Logger.h
+++ OpenGL/Log/Logger.h
@@ -2,6 +2,7 @@
 
 #include <string>
 #include <map>
+#include <vector>
 #include "ILogHandler.h"
 #include "ConsoleHandler.h"
 //#include "../Event/Event.h"
@@ -20,6 +21,7 @@ public:
 
 	ConsoleHandler& CreateConsoleHandler(std::string p_id);
 	ConsoleHandler& GetConsoleHandler(std::string p_id);
+	std::vector<LogData> GetAllLogData();
 
 public:
 	/*Event<const LogData&> LogEvent;*/
@@ -30,21 +32,9 @@ private:
 
 private:
 	std::map<std::string, ConsoleHandler> Console_Handle_Map;
+
+
+	std::vector<LogData> m_logData;
 };
 
-template<typename T>
-inline void Logger::LogToHandlerMap(std::map<std::string, T>& p_map, const LogData& p_data, std::string p_id)
-{
-	if (p_id != "default")
-		p_map[p_id].Log(p_data);
-	else
-	{
-		if (p_map.find("default") != p_map.end())
-			p_map["default"].Log(p_data);
-		else
-		{
-			p_map.emplace("default", T());
-			p_map["default"].Log(p_data);
-		}
-	}
-}
+#include "Logger.inl"
