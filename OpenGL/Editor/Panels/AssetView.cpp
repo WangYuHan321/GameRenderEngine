d63b50aa5d9d0467616a2af6146997058ff35ac5
WangYuHan <1269940762@qq.com>
2024-03-25 18:16:10 +0800

    modify assetview class

--- OpenGL/Editor/Panels/AssetView.cpp
+++ OpenGL/Editor/Panels/AssetView.cpp
@@ -1,1 +1,63 @@
 #include "AssetView.h"
+#include "../../Render/ForwardRenderer.h"
+#include "../../../Render/RenderTarget.h"
+#include "../../Editor/Core/EditorAction.h"
+#include "../../Core/ECS/Components/CModelRenderer.h"
+#include "../../Core/ECS/Components/CCamera.h"
+#include "../../UI/Visual/Image.h"
+#include "../../UI/Plugin/DDTarget.h"
+#include "../../File/Path/PathParser.h"
+
+AssetView::AssetView(
+	const std::string& p_title,
+	bool p_opened,
+	const PanelWindowSetting& p_windowSettings
+) : 
+	AViewControllable(p_title, p_opened, p_windowSettings)
+{
+	m_camera.ClearColor = (Color4( 0.098f, 0.098f, 0.098f, 1.0f ));
+	m_camera.Far = (5000.0f);
+
+	m_resource = static_cast<Model*>(nullptr);
+}
+
+void AssetView::_Render_Impl()
+{
+	PrepareCamera();
+
+	auto& baseRenderer = *dynamic_cast<ForwardRenderer*>(EDITOR_CONTEXT(m_renderer).get());
+
+	m_renderTarget->Bind();
+
+	baseRenderer.SetStencilMask(0xFF);
+	baseRenderer.Clear(m_camera);
+	baseRenderer.SetStencilMask(0x00);
+
+	uint8_t glState = baseRenderer.FetchGLState();
+	baseRenderer.ApplyStateMask(glState);
+
+	Vector3 c(0.176f, 0.176f, 0.176f);
+	m_editorRenderer.RenderGrid(m_camPos, c);
+
+	if (auto pval = std::get_if<Material*>(&m_resource); pval && *pval)
+		m_editorRenderer.RenderMaterialAsset(**pval);
+
+	baseRenderer.ApplyStateMask(glState);
+
+	m_renderTarget->Unbind();
+}
+
+void AssetView::SetResource(ViewableResource p_viewResource)
+{
+	m_resource = p_viewResource;
+}
+
+AssetView::~AssetView()
+{
+
+}
+
+AssetView::ViewableResource AssetView::GetResource() const
+{
+	return m_resource;
+}
--- OpenGL/Editor/Panels/AssetView.h
+++ OpenGL/Editor/Panels/AssetView.h
@@ -1,9 +1,29 @@
 #pragma once
 
+#include <variant>
 #include "AViewControllable.h"
+
+class Model;
+class Texture;
+class Material;
 
 class AssetView : public AViewControllable
 {
+public:
+	using ViewableResource = std::variant<Model*, Texture*, Material*>;
 
+	AssetView(
+		const std::string& p_title,
+		bool p_opened,
+		const PanelWindowSetting& p_windowSettings
+	);
+	~AssetView();
+	virtual void _Render_Impl() override;
+
+	void SetResource(ViewableResource p_viewResource);
+	ViewableResource GetResource() const;
+
+private:
+	ViewableResource m_resource;
 };
 
