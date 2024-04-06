92c5aee8317ec93be108cbe421452072dca0297e
WangYuHan <1269940762@qq.com>
2024-04-06 15:42:18 +0800

    modify class CascadeShadowMap

--- OpenGL/Render/Shadow/CascadeShadowMap.cpp
+++ OpenGL/Render/Shadow/CascadeShadowMap.cpp
@@ -1,6 +1,9 @@
 #include "CascadeShadowMap.h"
 #include <glm/gtc/matrix_transform.hpp>
 #include "../../Render/RenderTarget.h"
+#include "../../Camera/Camera.h"
+#include "../../Render/Mesh/Material.h"
+#include "../../Core/ECS/Components/CDirectionalLight.h"
 
 CascadeShadowMap::CascadeShadowMap()
 {
@@ -14,7 +17,7 @@ CascadeShadowMap::CascadeShadowMap()
 void CascadeShadowMap::InitShadowMap(int level, float minX, float maxX, float miny, float maxY, float minz, float maxZ)
 {
 	m_pShadowMap[level].proj = glm::ortho<float>(minX, maxX, miny, maxY, minz, maxZ);
-	RenderTarget* rt = new RenderTarget(2048, 2048, GL_UNSIGNED_BYTE, 0, true);
+	RenderTarget* rt = new RenderTarget(1024, 1024, GL_UNSIGNED_BYTE, 0, true);
 	rt->GetDepthStencilTexture()->Bind();
 	rt->GetDepthStencilTexture()->SetFilterMin(GL_NEAREST);
 	rt->GetDepthStencilTexture()->SetFilterMax(GL_NEAREST);
@@ -23,6 +26,51 @@ void CascadeShadowMap::InitShadowMap(int level, float minX, float maxX, float mi
 	//???? glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER, border);
 	glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, border);
 	m_pShadowMap[level].shadowMap = rt;
+}
+
+void CascadeShadowMap::BeginShadowRender(int level)
+{
+	currentLevel = level;
+	glBindFramebuffer(GL_FRAMEBUFFER, m_pShadowMap[currentLevel].shadowMap->ID);
+	glClear(GL_DEPTH_BUFFER_BIT);
+}
+
+void CascadeShadowMap::EndFrame(Material* p_material)
+{
+	if (p_material)
+	{
+		for (unsigned int i = 0; i < 5; i++)
+		{
+			p_material->GetShader()->SetMatrix("shadow_lightDepthMat" + std::to_string(i), m_pShadowMap[i].depth);
+			p_material->SetTextureValue("shadow_LightDepthMap" + std::to_string(i), m_pShadowMap[i].shadowMap->GetDepthStencilTexture());
+		}
+		p_material->SetBoolean("shadow_shadowCast", p_material->ShadowCast);
+	}
+	glBindFramebuffer(GL_FRAMEBUFFER, previousFrameBuffer);
+}
+
+Matrix4 CascadeShadowMap::GetCurDepthMatrix4()
+{
+	return m_pShadowMap[currentLevel].depth;
+}
+
+void CascadeShadowMap::EndShadowRender()
+{
+	m_pShadowMap[currentLevel].depth = biasMatrix * m_pShadowMap[currentLevel].depth;
+}
+
+void CascadeShadowMap::BeginFrame(CDirectionalLight p_dirLight,const Camera* p_cam)
+{
+	const glm::vec3& cameraPosition = p_cam->Position;
+	glm::vec3 target = glm::vec3(-cameraPosition.x, 0, -cameraPosition.z);
+	glm::mat4 depthViewMatrix = glm::lookAt(target + (p_dirLight.GetDirectional() * 50.0f), target, glm::vec3(0, 1, 0));
+
+	for (unsigned int i = 0; i < 5; i++)
+	{
+		m_pShadowMap[i].depth = m_pShadowMap[i].proj * depthViewMatrix;
+	}
+
+	glGetIntegerv(GL_FRAMEBUFFER_BINDING, &previousFrameBuffer);
 }
 
 CascadeShadowMap::~CascadeShadowMap()
--- OpenGL/Render/Shadow/CascadeShadowMap.h
+++ OpenGL/Render/Shadow/CascadeShadowMap.h
@@ -2,7 +2,10 @@
 
 #include "../../../Util/common.h"
 
+class Camera;
+class Material;
 class RenderTarget;
+class CDirectionalLight;
 
 class CascadeShadowMap
 {
@@ -22,8 +25,17 @@ private:
 	Matrix4 biasMatrix;
 
 public:
+	int previousFrameBuffer;
+
 	CascadeShadowMap();
 	~CascadeShadowMap();
+
+	void BeginFrame(CDirectionalLight p_dirLight, const Camera* p_cam);
+	void BeginShadowRender(int level);
+	void EndShadowRender();
+	void EndFrame(Material* p_material = nullptr);
+	Matrix4 GetCurDepthMatrix4();
+
 
 	void InitShadowMap(int level, float minX, float maxX, float miny, float maxY, float minz, float maxZ);
 };
