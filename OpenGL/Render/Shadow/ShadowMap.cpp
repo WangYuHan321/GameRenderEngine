0cea99dbb2baa3cccbcf19bd4a84af87dd0ff74b
WangYuHan <1269940762@qq.com>
2024-04-11 23:58:46 +0800

    add class shadowmap

--- OpenGL/Render/Shadow/ShadowMap.cpp
+++ OpenGL/Render/Shadow/ShadowMap.cpp
@@ -25,13 +25,68 @@ ShadowMap::~ShadowMap()
 		delete m_renderTarget;
 }
 
+void ShadowMap::Clear()
+{
+	glGetIntegerv(GL_FRAMEBUFFER_BINDING, &previousFrameBuffer);
+	glBindFramebuffer(GL_FRAMEBUFFER, m_renderTarget->ID);
+	glClear(GL_DEPTH_BUFFER_BIT);
+	glBindFramebuffer(GL_FRAMEBUFFER, previousFrameBuffer);
+}
+
+void ShadowMap::InitializeFrame(CDirectionalLight dirLight, const Camera* p_cam)
+{
+	const auto _size = 20.0f;
+
+	const auto _near = 0.1f;
+	const auto _far = 20.0f;
+	glm::mat4 lightProject = glm::ortho(-20.0f, 20.0f, 20.0f, -20.0f, _near, _far);
+	glm::mat4 lightView = glm::lookAt(-dirLight.GetDirectional() * 10.0f, glm::vec3(0.0, 0.0, 0.0), glm::vec3(0.0, 1.0, 0.0));
+	m_lightMatrix = lightProject * lightView;
+
+	auto [t, r, s] = DecomposeTransform(m_lightMatrix);
+
+	const auto right = 1.0 * _size;
+	const auto left = -right;
+	const auto top = _size;
+	const auto bottom = -top;
+
+	const auto a = r * Vector3 { left, top, 0 };
+	const auto b = r * Vector3 { right, top, 0 };
+	const auto c = r * Vector3 { left, bottom, 0 };
+	const auto d = r * Vector3 { right, bottom, 0 };
+	m_debugOrtho = { -dirLight.GetDirectional() * 10.0f, glm::normalize((glm::vec3(0.0) - (-dirLight.GetDirectional() * 10.0f))), _near, _far, a, b, c, d };
+}
+
+void ShadowMap::SetShadowMap(Material* p_material)
+{
+	p_material->SetTextureValue("shadow_LightDepthMap0", m_renderTarget->GetDepthStencilTexture());
+	p_material->GetShader()->SetMatrix("shadow_lightDepthMat0", m_lightMatrix);
+	p_material->SetBoolean("shadow_shadowReceive", p_material->ShadowReceive);
+}
+
 void ShadowMap::BeginShadow()
 {
 	glGetIntegerv(GL_FRAMEBUFFER_BINDING, &previousFrameBuffer);
 	glBindFramebuffer(GL_FRAMEBUFFER, m_renderTarget->ID);
+	glViewport(0, 0, m_renderTarget->Width, m_renderTarget->Height);
 }
 
 void ShadowMap::EndShadow()
 {
 	glBindFramebuffer(GL_FRAMEBUFFER, previousFrameBuffer);
+}
+
+Matrix4 ShadowMap::GetCurDepthMatrix4()
+{
+	return m_lightMatrix;
+}
+
+int ShadowMap::GetShadowMap(int id)
+{
+	return m_renderTarget->GetDepthStencilTexture()->ID;
+}
+
+DebugOrthographics ShadowMap::GetOrthographicFrustum(int curLevel)
+{
+	return m_debugOrtho;
 }
--- OpenGL/Render/Shadow/ShadowMap.h
+++ OpenGL/Render/Shadow/ShadowMap.h
@@ -1,7 +1,12 @@
 #pragma once
 
+#include "../../Util/common.h"
+#include "CascadeShadowMap.h"
+
 class RenderTarget;
 class CDirectionalLight;
+class Material;
+class Camera;
 
 class ShadowMap
 {
@@ -11,14 +16,23 @@ public:
 	ShadowMap();
 	~ShadowMap();
 
+	void Clear();
+	void InitializeFrame(CDirectionalLight dirLight, const Camera* p_cam);
+	void SetShadowMap(Material* p_materix);
 	void BeginShadow();
 	void EndShadow();
+
+	Matrix4 GetCurDepthMatrix4();
+	int GetShadowMap(int id);
+	DebugOrthographics GetOrthographicFrustum(int curLevel = 0);
 
 	int previousFrameBuffer;
 
 private:
 
+	Matrix4 m_lightMatrix;
 	RenderTarget* m_renderTarget;
+	DebugOrthographics m_debugOrtho;
 
 };
 
