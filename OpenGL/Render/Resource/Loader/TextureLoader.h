fb5a58b13568445cde0d714420af5d7ed0c783a0
WangYuHan <1269940762@qq.com>
2024-04-19 04:12:59 +0800

    add Create Function

--- OpenGL/Render/Resource/Loader/TextureLoader.h
+++ OpenGL/Render/Resource/Loader/TextureLoader.h
@@ -15,6 +15,8 @@ public:
 	Texture* LoadHDRTexture(std::string path);
 
 	Texture* CreateColor(uint32_t p_data, GLenum p_firstFilter, GLenum p_secondFilter, bool p_generateMipmap);
+	//font
+	Texture* Create(uint32 width, uint32 height, GLenum internalFormat, GLenum format, GLenum type, void* data);
 
 	Texture LoadTexture1(std::string path, GLenum Target, GLenum internalFormat, bool srgb = false);//¼æÈÝÎÞ±à¼­Æ÷Ä£Ê½
 	Texture LoadHDRTexture1(std::string path);
