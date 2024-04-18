7104e3bf86cb98ed8ac824d5875a80479f81f263
WangYuHan <1269940762@qq.com>
2024-04-19 04:07:49 +0800

    add Destory Actor Attach Dettach Event

--- OpenGL/Editor/Panels/Hierarchy.cpp
+++ OpenGL/Editor/Panels/Hierarchy.cpp
@@ -1,11 +1,15 @@
 #include "Hierarchy.h"
 
 #include <algorithm>
+#include "MaterialEditor.h"
 #include "../../Core/ECS/Actor.h"
 #include "../../UI/Plugin/ContextualMenu.h"
+#include "../../UI/Plugin/DDSource.h"
 #include "../../UI/Plugin/DDTarget.h"
 #include "../../Editor/Core/EditorAction.h"
 #include "../../UI/Widgets/InputField/InputText.h"
+#include "../../Editor/Core/EditorAction.h"
+#include "../../Core/ECS/Components/CMaterialRenderer.h"
 
 class HierarchyContextualMenu : public ContextualMenu
 {
@@ -16,6 +20,16 @@ public:
 	{
 		if (m_target)
 		{
+			auto& editorActorBtn = CreateWidget<MenuItem>("Editor Material");
+			editorActorBtn.ClickedEvent += [this]
+			{
+				if (m_target->GetComponent<CMaterialRenderer>() != nullptr)
+				{
+					// wirte on modify
+					EDITOR_PANEL(MaterialEditor, "MaterialEditor").SetOpened(true);
+					EDITOR_PANEL(MaterialEditor, "MaterialEditor").SetTarget(m_target->GetComponent<CMaterialRenderer>()->GetMaterialList()[0]);
+				}
+			};
 
 		}
 
@@ -81,18 +95,132 @@ Hierarchy::Hierarchy(const std::string& p_title,
 	m_sceneRoot->AddPlugin<HierarchyContextualMenu>(nullptr, *m_sceneRoot);
 
 	Actor::CreateEvent += std::bind(&Hierarchy::AddActorByInstance, this, std::placeholders::_1);
+	Actor::DestroyedEvent += std::bind(&Hierarchy::DestroyActor, this, std::placeholders::_1);
+	Actor::AttachEvent += std::bind(&Hierarchy::AttachActor, this, std::placeholders::_1, std::placeholders::_2);
+	Actor::DettachEvent += std::bind(&Hierarchy::DetachActor, this, std::placeholders::_1, std::placeholders::_2);
+}
 
+void Hierarchy::AddContextualMenu(TreeNode& treeNode, Actor& p_actor)
+{
+	treeNode.AddPlugin<HierarchyContextualMenu>(&p_actor, treeNode);
+}
+
+void Hierarchy::AddDDTargetFunction(TreeNode& treeNode, Actor& p_actor)
+{
+	treeNode.AddPlugin<DDTarget<Actor*>>("Scene_Actor").DataReceivedEvent += [&treeNode, &p_actor, this](Actor* p_otherActor)
+	{
+		LOG_INFO(p_otherActor->GetName());
+		p_otherActor->SetParent(p_actor);
+
+		bool isLeaf = m_widgetActorLink[p_otherActor]->leaf;
+		m_sceneRoot->RemoveWidget(*m_widgetActorLink[p_otherActor]);
+		std::unordered_map<Actor*, TreeNode*>::iterator it = m_widgetActorLink.find(p_otherActor);
+		if (it != m_widgetActorLink.end()) {
+			m_widgetActorLink[&p_actor]->leaf = false;
+			m_widgetActorLink[p_otherActor] = &m_widgetActorLink[&p_actor]->CreateWidget<TreeNode>(p_otherActor->GetName(), true);
+
+			AddContextualMenu(*m_widgetActorLink[p_otherActor], *p_otherActor);
+			AddDDTargetFunction(*m_widgetActorLink[p_otherActor], *p_otherActor);
+			AddDDSourceFunction(*m_widgetActorLink[p_otherActor], p_otherActor->GetName(), *p_otherActor);
+
+			m_widgetActorLink[p_otherActor]->leaf = isLeaf;
+		}
+	};
+}
+
+void Hierarchy::AddDDSourceFunction(TreeNode& treeNode, std::string& name, Actor& p_actor)
+{
+	treeNode.AddPlugin<DDSource<Actor*>>("Scene_Actor", name, &p_actor);
 }
 
 void Hierarchy::AddActorByInstance(Actor& p_actor)
 {
-	auto& textSelectable = m_sceneRoot->CreateWidget<TreeNode>(p_actor.GetName(), true);
-	textSelectable.leaf = true;//是叶子节点
-	textSelectable.AddPlugin<HierarchyContextualMenu>(&p_actor, textSelectable);
+	if (p_actor.HasParent())
+	{
+		if(m_widgetActorLink.find(p_actor.GetParent()) != m_widgetActorLink.end())
+		{
+			//if contain widget Actor
+			auto& textSelectable = m_widgetActorLink[p_actor.GetParent()]->CreateWidget<TreeNode>(p_actor.GetName(), true);
+			textSelectable.leaf = true;//是叶子节点
+			textSelectable.AddPlugin<HierarchyContextualMenu>(&p_actor, textSelectable);
 
-	m_widgetActorLink[&p_actor] = &textSelectable;
+			m_widgetActorLink[&p_actor] = &textSelectable;
+		}
+		else
+		{
+			auto& parentSelectable = m_sceneRoot->CreateWidget<TreeNode>(p_actor.GetParent()->GetName(), true);
+			parentSelectable.leaf = true;//是叶子节点
+			parentSelectable.AddPlugin<HierarchyContextualMenu>(&p_actor, parentSelectable);
 
-	textSelectable.ClickedEvent += EDITOR_BIND(SelectActor, std::ref(p_actor));
+			m_widgetActorLink[p_actor.GetParent()] = &parentSelectable;
+
+			auto& textSelectable = parentSelectable.CreateWidget<TreeNode>(p_actor.GetName(), true);
+			textSelectable.leaf = true;//是叶子节点
+			textSelectable.AddPlugin<HierarchyContextualMenu>(&p_actor, textSelectable);
+
+			m_widgetActorLink[&p_actor] = &parentSelectable;
+		}
+	}
+	else
+	{
+		auto& textSelectable = m_sceneRoot->CreateWidget<TreeNode>(p_actor.GetName(), true);
+		textSelectable.leaf = true;//是叶子节点
+
+		AddContextualMenu(textSelectable , p_actor);
+		AddDDSourceFunction(textSelectable, p_actor.GetName(), p_actor);
+		AddDDTargetFunction(textSelectable, p_actor);
+
+		m_widgetActorLink[&p_actor] = &textSelectable;
+
+		textSelectable.ClickedEvent += EDITOR_BIND(SelectActor, std::ref(p_actor));
+	}
+
+}
+
+void Hierarchy::DestroyActor(Actor& p_actor)
+{
+	std::unordered_map<Actor*, TreeNode*>::iterator it = m_widgetActorLink.find(&p_actor);
+	if (it != m_widgetActorLink.end()) 
+	{
+		if (p_actor.GetParent() != nullptr)
+			m_widgetActorLink[p_actor.GetParent()]->RemoveWidget(*m_widgetActorLink[&p_actor]);
+		else
+			m_sceneRoot->RemoveWidget(*m_widgetActorLink[&p_actor]);
+	}
+}
+
+void Hierarchy::AttachActor(Actor& p_actor, Actor& p_other)//actor to parent
+{
+	bool isLeaf = m_widgetActorLink[&p_actor]->leaf;
+	m_sceneRoot->RemoveWidget(*m_widgetActorLink[&p_actor]);
+	std::unordered_map<Actor*, TreeNode*>::iterator it = m_widgetActorLink.find(&p_other);
+	if (it != m_widgetActorLink.end()) {
+		m_widgetActorLink[&p_other]->leaf = false;
+		m_widgetActorLink[&p_actor] = &m_widgetActorLink[&p_other]->CreateWidget<TreeNode>(p_actor.GetName(), true);
+
+		AddContextualMenu(*m_widgetActorLink[&p_actor], p_actor);
+		AddDDTargetFunction(*m_widgetActorLink[&p_actor], p_actor);
+		AddDDSourceFunction(*m_widgetActorLink[&p_actor], p_actor.GetName(), p_actor);
+
+		m_widgetActorLink[&p_actor]->leaf = isLeaf;
+	}
+
+}
+
+void Hierarchy::DetachActor(Actor& p_actor, Actor& p_parent)
+{
+	std::unordered_map<Actor*, TreeNode*>::iterator it = m_widgetActorLink.find(&p_parent);
+	if (it != m_widgetActorLink.end()) {
+		m_widgetActorLink[&p_parent]->RemoveWidget(*m_widgetActorLink[&p_actor]);
+		m_widgetActorLink[&p_actor] = &m_sceneRoot->CreateWidget<TreeNode>(p_actor.GetName());
+		
+		AddContextualMenu(*m_widgetActorLink[&p_actor], p_actor);
+		AddDDTargetFunction(*m_widgetActorLink[&p_actor], p_actor);
+		AddDDSourceFunction(*m_widgetActorLink[&p_actor], p_actor.GetName(), p_actor);
+
+		m_widgetActorLink[&p_actor]->leaf = true;
+	}
+
 }
 
 
--- OpenGL/Editor/Panels/Hierarchy.h
+++ OpenGL/Editor/Panels/Hierarchy.h
@@ -14,6 +14,14 @@ public:
 		const PanelWindowSetting p_windowSetting);
 
 	void AddActorByInstance(Actor& p_actor);
+	void DestroyActor(Actor& p_actor);
+	void AttachActor(Actor& p_actor, Actor& p_other);
+	void DetachActor(Actor& p_actor, Actor& p_parent);
+
+private:
+	void AddContextualMenu(TreeNode& treeNode, Actor& p_actor);
+	void AddDDTargetFunction(TreeNode& treeNode, Actor& p_actor);
+	void AddDDSourceFunction(TreeNode& treeNode, std::string& name, Actor& p_actor);
 
 public:
 	Event<Actor&> ActorSelectedEvemt;
