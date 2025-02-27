b0ef2a98171eb55977cc12f4fb7c92f7dbdfb52a
wangyuhan <1269940762@qq.com>
2025-02-27 15:40:03 +0800

    [modify] modify support mult platform .

--- OpenGL/File/ConfigManager.cpp
+++ OpenGL/File/ConfigManager.cpp
@@ -2,6 +2,15 @@
 #include <cstdlib>
 #include <filesystem>
 
+string PathConCatDirAndFile(string srcDir, string dstDir)
+{
+	std::filesystem::path srcPath = srcDir;
+	std::filesystem::path dstPath = dstDir;
+	std::string path = std::filesystem::absolute((srcPath / dstPath)).string();
+	
+	return path;
+}
+
 ConfigManager::ConfigManager()
 {
 
@@ -15,45 +24,42 @@ ConfigManager::~ConfigManager()
 std::string ConfigManager::GetExecutePath() const
 {
 	char const* envRoot = getenv("ASSIMP");
-	std::string path = "";
-	if (envRoot != nullptr)
-		path = std::string(envRoot) + "/../../../Bin";
-	else
-		path = std::filesystem::current_path().generic_string();
+	string path = "";
+	path = std::filesystem::current_path().generic_string();
 	return path;
 }
 
 std::string ConfigManager::GetEnginePath() const
 {
 	std::string exePath = GetExecutePath();
-	std::string path = exePath + "/Data/Engine";
+	std::string path = PathConCatDirAndFile(exePath, "Data/Engine");
 	return path;
 }
 
 std::string ConfigManager::GetEditorPath() const
 {
 	std::string exePath = GetExecutePath();
-	std::string path = exePath + "/Data/Editor";
+	std::string path = PathConCatDirAndFile(exePath, "Data/Editor");
 	return path;
 }
 
 std::string ConfigManager::GetFontPath() const
 {
 	std::string enginePath = GetEnginePath();
-	std::string path = enginePath + "/Font";
+	std::string path = PathConCatDirAndFile(enginePath, "Font");
 	return path;
 }
 
 std::string ConfigManager::GetScriptPath() const
 {
 	std::string editorPath = GetEditorPath();
-	std::string path = editorPath + "/Script";
+	std::string path = PathConCatDirAndFile(editorPath, "Script");
 	return path;
 }
 
 std::string ConfigManager::GetXmlPath() const
 {
-	return GetEditorPath() + "/Xml";
+	return PathConCatDirAndFile(GetEditorPath(), "Xml");
 }
 
 std::string ConfigManager::GetProjectPath() const
